7ddc19b1846baa0b1d976cff23fc3483cab5ec7b
Chaitanya Murarka <103809867+chaitanyamurarka@users.noreply.github.com>
2025-07-10 12:47:48 +0000

    Improved Bat File

--- run_microservices.bat
+++ run_microservices.bat
@@ -1,18 +1,12 @@
 @echo off
-start python Microservices\Port8007.py
-timeout /t 1 /nobreak > nul
-start python Microservices\Port8006.py
-timeout /t 1 /nobreak > nul
-start python Microservices\Port8005.py
-timeout /t 1 /nobreak > nul
-start python Microservices\Port8004.py
-timeout /t 1 /nobreak > nul
-start python Microservices\Port8003.py
-timeout /t 1 /nobreak > nul
-start python Microservices\Port8002.py
-timeout /t 1 /nobreak > nul
-start python Microservices\Port8001.py
-timeout /t 1 /nobreak > nul
-start python Microservices\Port8000.py
+
+REM Activate the virtual environment
+call "%~dp0.venv\Scripts\activate.bat"
+
+REM Run microservices
+for %%f in (Port8000.py Port8001.py Port8002.py Port8003.py Port8004.py Port8005.py Port8006.py Port8007.py) do (
+    start "" "%~dp0.venv\Scripts\python.exe" "Microservices\%%f"
+    timeout /t 1 /nobreak > nul
+)
 
 pause
